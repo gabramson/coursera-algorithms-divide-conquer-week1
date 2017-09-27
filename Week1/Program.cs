cc70dc9b85f9025e7b70e2d87c4675e6cef77fc9
Gary Abramson <gary.abramson@gmail.com>
2017-09-27 01:01:01 -0500

    Got right answer

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -6,7 +6,10 @@ namespace Week1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(BigMath.Multiply(
+                "3141592653589793238462643383279502884197169399375105820974944592",
+                "2718281828459045235360287471352662497757247093699959574966967627"));
+            Console.ReadKey();
         }
     }
 }
